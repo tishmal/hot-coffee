b491892e68758f0a2ef85fe37af8613ddc1dfeab
asarmukh <ayansarmukhanovv@gmail.com>
2025-01-26 19:34:00 +0500

    add delete order function

--- internal/dal/order_repository.go
+++ internal/dal/order_repository.go
@@ -4,14 +4,31 @@ import (
 	"encoding/json"
 	"fmt"
 	"hot-coffee/models"
+	"io/ioutil"
+	"log"
 	"os"
 )
+
+type OrderRepositoryInterface interface {
+	CreateOrder(order models.Order) error
+	LoadOrders() ([]models.Order, error)
+	GetOrderByID(id string) (*models.Order, error)
+	DeleteOrder(id string) (*models.Order, error)
+}
+
+type OrderRepositoryJSON struct {
+	filePath string
+}
+
+func NewOrderRepositoryJSON(filePath string) *OrderRepositoryJSON {
+	return &OrderRepositoryJSON{filePath: filePath}
+}
 
 const ordersFile = "data/orders.json"
 
 // Сохранение заказа в JSON файл
-func SaveOrder(order models.Order) error {
-	orders, err := loadOrders()
+func (r *OrderRepositoryJSON) CreateOrder(order models.Order) error {
+	orders, err := r.LoadOrders()
 	if err != nil {
 		return err
 	}
@@ -19,22 +36,6 @@ func SaveOrder(order models.Order) error {
 	orders = append(orders, order)
 
 	return saveOrders(orders)
-}
-
-// Загрузка всех заказов из файла
-func loadOrders() ([]models.Order, error) {
-	file, err := os.Open(ordersFile)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	var orders []models.Order
-	if err := json.NewDecoder(file).Decode(&orders); err != nil {
-		return nil, err
-	}
-
-	return orders, nil
 }
 
 // Запись заказов в файл
@@ -52,4 +53,89 @@ func saveOrders(orders []models.Order) error {
 	}
 
 	return nil
+}
+
+// Загрузка всех заказов из файла
+func (r *OrderRepositoryJSON) LoadOrders() ([]models.Order, error) {
+	file, err := os.Open(ordersFile)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var orders []models.Order
+	if err := json.NewDecoder(file).Decode(&orders); err != nil {
+		return nil, err
+	}
+
+	return orders, nil
+}
+
+func (r *OrderRepositoryJSON) GetOrderByID(id string) (*models.Order, error) {
+	// Открываем файл с данными
+	data, err := ioutil.ReadFile("data/orders.json")
+	if err != nil {
+		return nil, fmt.Errorf("Error reading file: %v", err)
+	}
+
+	// Слайс для хранения всех заказов
+	var orders []models.Order
+
+	// Парсим JSON из файла в структуру
+	err = json.Unmarshal(data, &orders)
+	if err != nil {
+		log.Fatalf("Error unmarshaling JSON: %v", err)
+	}
+
+	// Проверяем, что заказы есть в данных
+	if len(orders) > 0 {
+		for i := 0; i < len(orders); i++ {
+			if orders[i].ID == id {
+				return &orders[i], nil
+			}
+		}
+	}
+	return nil, fmt.Errorf("Order with ID %s not found", id)
+}
+
+func (r *OrderRepositoryJSON) DeleteOrder(id string) (*models.Order, error) {
+	// Открываем файл с данными
+	data, err := ioutil.ReadFile("data/orders.json")
+	if err != nil {
+		return nil, fmt.Errorf("Error reading file: %v", err)
+	}
+
+	// Слайс для хранения всех заказов
+	var orders []models.Order
+
+	// Парсим JSON из файла в структуру
+	err = json.Unmarshal(data, &orders)
+	if err != nil {
+		log.Fatalf("Error unmarshaling JSON: %v", err)
+	}
+
+	// Проверяем, что заказы есть в данных
+	for i := 0; i < len(orders); i++ {
+		if orders[i].ID == id {
+			deletedOrder := orders[i]
+
+			// Удаляем заказ из слайса
+			orders = append(orders[:i], orders[i+1:]...)
+
+			// Перезаписываем файл с обновленным списком заказов
+			updatedData, err := json.MarshalIndent(orders, "", "  ")
+			if err != nil {
+				return nil, fmt.Errorf("Error marshaling updated orders: %v", err)
+			}
+
+			err = ioutil.WriteFile("data/orders.json", updatedData, os.ModePerm)
+			if err != nil {
+				return nil, fmt.Errorf("Error writing updated file: %v", err)
+			}
+
+			// Возвращаем удаленный заказ
+			return &deletedOrder, nil
+		}
+	}
+	return nil, fmt.Errorf("Order with ID %s not found", id)
 }
--- internal/handler/order_handler.go
+++ internal/handler/order_handler.go
@@ -7,8 +7,23 @@ import (
 	"net/http"
 )
 
+type OrderHandlerInterface interface {
+	HandleCreateOrder(w http.ResponseWriter, r *http.Request)
+	HandleGetAllOrders(w http.ResponseWriter, r *http.Request)
+	HandleGetOrderById(w http.ResponseWriter, r *http.Request)
+	HandleDeleteOrder(w http.ResponseWriter, r *http.Request)
+}
+
+type OrderHandler struct {
+	orderService service.OrderService
+}
+
+func NewOrderHandler(orderService service.OrderService) *OrderHandler {
+	return &OrderHandler{orderService: orderService}
+}
+
 // Обработчик для создания нового заказа
-func CreateOrder(w http.ResponseWriter, r *http.Request) {
+func (h *OrderHandler) HandleCreateOrder(w http.ResponseWriter, r *http.Request) {
 	var newOrder models.Order
 	if err := json.NewDecoder(r.Body).Decode(&newOrder); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
@@ -16,7 +31,7 @@ func CreateOrder(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Вызываем сервис для создания заказа
-	if err := service.CreateOrder(newOrder); err != nil {
+	if err := h.orderService.CreateOrder(newOrder); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -26,3 +41,33 @@ func CreateOrder(w http.ResponseWriter, r *http.Request) {
 }
 
 // Дополнительные обработчики для получения, обновления и удаления заказов
+
+func (h *OrderHandler) HandleGetAllOrders(w http.ResponseWriter, r *http.Request) {
+	orders, err := h.orderService.GetAllOrders()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(orders)
+}
+
+func (h *OrderHandler) HandleGetOrderById(w http.ResponseWriter, r *http.Request, orderID string) {
+	order, err := h.orderService.GetOrderByID(orderID)
+	if err != nil || &order == nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(order)
+}
+
+func (h *OrderHandler) HandleDeleteOrder(w http.ResponseWriter, r *http.Request, orderID string) {
+	order, err := h.orderService.DeleteOrder(orderID)
+	if err != nil || &order == nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(order)
+}
